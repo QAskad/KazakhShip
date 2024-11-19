4ef5d843741a67124d20ab9968ce1442541e7179
QAskad <230536@astanait.edu.kz>
2024-11-19 07:11:56 +0500

    normal commit

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:642324@localhost/ks'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- ks/__init__.py
+++ ks/__init__.py
@@ -1,21 +1,16 @@
-# __init__.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 
-db = SQLAlchemy()
-login_manager = LoginManager()
+db = SQLAlchemy()  # Создаем объект для работы с базой данных
 
 def create_app():
-    app = Flask(__name__)
-    app.config.from_object('config.Config')
+    app = Flask(__name__)  # Создаем Flask приложение
+    app.config.from_object('config.Config')  # Загружаем конфигурацию из config.py
 
-    db.init_app(app)
-    login_manager.init_app(app)
+    db.init_app(app)  # Инициализируем объект базы данных с приложением
 
-    # Пример добавления API
+    # Импортируем и регистрируем Blueprint для API
     from .api import api
-    app.register_blueprint(api, url_prefix='/api')
+    app.register_blueprint(api, url_prefix='/api')  # Регистрируем Blueprint api
 
     return app
-
